0ee0256db76ca3503c277c3d0bbe7df3a107a614
Busola5713 <badmusbusola@gmail.com>
2024-06-20 10:49:27 -0400

    finished

---
+++ while_loop_flag_version.py
@@ -0,0 +1,17 @@
+def get_starting_number():
+        bottles = input("How many bottles of beer on the wall? ")
+        if bottles >= "1":
+            return int(bottles)
+
+
+def sing(): 
+    beer = get_starting_number()
+    i = beer
+    while i > 0:
+       if i == 1:
+            print(f"{i} bottle of beer on the wall, {i} bottle of beer.\nTake one down, pass it around, no more bottles of beer on the wall!\n")
+       elif i == 2:
+            print(f"{i} bottle of beer on the wall, {i} bottle of beer.\nTake one down, pass it around, {i-1} bottle of beer on the wall.\n")
+       else:
+            print(f"{i} bottles of beer on the wall, {i} bottles of beer.\nTake one down, pass it around, {i-1} bottles of beer on the wall.\n")
+       i -= 1
